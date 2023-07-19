58ed4944e57428ca10c9e4c90f75602e6aba4a26
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-07-19 22:42:37 +0530

    add boilerplate head and body component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;

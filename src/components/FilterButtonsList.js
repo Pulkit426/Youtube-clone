f0b86042cd87862c37add18f7f90994484d2921b
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-07-20 20:34:39 +0530

    add filter buttons

---
+++ src/components/FilterButtonsList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const FilterButtonsList = () => {
+  const dataList = [
+    "All",
+    "Sports",
+    "Cricket",
+    "Comedy",
+    "Trailers",
+    "Podcasts",
+    "Coding",
+  ];
+  return (
+    <div>
+      {dataList.map((item) => (
+        <button className="p-3 m-2 border border-black-50 rounded-md bg-gray-100">
+          {item}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FilterButtonsList;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FilterButtonsList from "./FilterButtonsList";
+
+const MainContainer = () => {
+  return (
+    <div className="col-span-10">
+      <FilterButtonsList />
+    </div>
+  );
+};
+
+export default MainContainer;

29a2b9be7860e92104bce091260f2904105e9d5c
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-07-21 18:48:18 +0530

    add link for home page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const isMenuActive = useSelector((store) => store.app.isMenuOpen);
@@ -9,7 +10,9 @@ const Sidebar = () => {
   return (
     <div className="shadow-lg p-2 m-2 col-span-1">
       <ul>
-        <li>Home </li>
+        <Link to="/">
+          <li>Home </li>
+        </Link>
         <li>Shorts </li>
         <li>Subscriptions </li>
         <li>Library </li>

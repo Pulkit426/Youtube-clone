350316cdc18a55f5cfd957214e37406a1f374184
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-07-21 17:52:21 +0530

    set up routes using react router dom

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Header />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );

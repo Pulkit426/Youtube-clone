966d527d17469a566961138c843e491e01736c5b
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-08-03 22:05:09 +0530

    update video suggestion sidebar component

--- src/components/VideoSuggestionsSidebar.js
+++ src/components/VideoSuggestionsSidebar.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from "react";
-import VideoCard from "./VideoCard";
+import SuggestedVideoCard from "./SuggestedVideoCard";
 
 const VideoSuggestionsSidebar = ({ videoId }) => {
-  const [data, setData] = useState([]);
+  const [videoData, setVideoData] = useState([]);
+
   useEffect(() => {
     getVideoSuggestions();
   }, []);
 
   const getVideoSuggestions = async () => {
     const data = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=10&key=AIzaSyCGe2AQZKgHJAiLwHXPg_EAziT0iYTWLjM`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=15&key=AIzaSyCGe2AQZKgHJAiLwHXPg_EAziT0iYTWLjM`
     );
     const json = await data.json();
-    setData(json.items);
-
-    console.log("DATA RELATED - ", json.items);
+    setVideoData(json.items);
   };
+
   return (
-    <div>
-      SIDEBAR
-      {data?.map((item) => (
-        <VideoCard videoData={item} />
+    <div className="p-2 m-3 w-96">
+      {videoData?.map((item) => (
+        <SuggestedVideoCard videoData={item} />
       ))}
     </div>
   );

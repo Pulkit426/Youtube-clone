9a3aee3cb73281e2befac54fb6788b23e7de0020
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-08-08 23:01:06 +0530

    update api url for cors issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const YOUTUBE_API_KEY = "AIzaSyCGe2AQZKgHJAiLwHXPg_EAziT0iYTWLjM";
 
 export const YOUTUBE_API_URL =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+  "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
   YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API =

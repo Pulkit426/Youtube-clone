98c28d042166abd81a9ab6432e8b6e96830982c2
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-08-11 23:09:41 +0530

    add corsproxy for all api urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,20 +1,27 @@
 const YOUTUBE_API_KEY = "AIzaSyCGe2AQZKgHJAiLwHXPg_EAziT0iYTWLjM";
 
+const CORS_PROXY = "https://corsproxy.io/?";
+
 export const YOUTUBE_API_URL =
-  "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+  CORS_PROXY +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
   YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API =
+  CORS_PROXY +
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_RESULTS_API =
+  CORS_PROXY +
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
   YOUTUBE_API_KEY;
 
 export const YOUTUBE_RELATED_VIDEOS_API =
+  CORS_PROXY +
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=15&key=" +
   YOUTUBE_API_KEY;
 
 export const YOUTUBE_SPECIFIC_VIDEO_DATA_API =
+  CORS_PROXY +
   "https://www.googleapis.com/youtube/v3/videos?part=snippet&key=" +
   YOUTUBE_API_KEY;
